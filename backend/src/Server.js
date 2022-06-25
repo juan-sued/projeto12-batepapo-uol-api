9ba9e2f708a3f4957091d521af52ea7f7a9464f5
juansued1233536 <92616927+juansued1233536@users.noreply.github.com>
2022-06-25 19:54:54 -0300

    versão com cadastro de usuário

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -1,42 +1,77 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import chalk from 'chalk';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
+import joi from 'joi';
 
-const client = new MongoClient('mongodb://127.0.0.1:27017');
+//problema na parte de validação
+// ta dando erro na hora de registrar participante
+
+dotenv.config();
+const client = new MongoClient(process.env.URL_CONNECT_MONGO);
+
 let db;
 
-client.connect().then(() => {
-  db = client.db('uoldb');
-});
+db = client.db('uoldb');
 
 const server = express();
 
 server.use(cors());
 server.use(express.json());
 
-server.post('/participants', (request, response) => {
-  const participant = request.body;
-
-  if (!participant) response.status(422).send('O campo nome deve ser preenchido');
-
-  db.collection('participants')
-    .insertOne(participant)
-    .then(() => {
-      response.send(201);
-    });
-
-  response.send('tudo ok por aqui');
+const participantSchema = joi.object({
+  name: joi.string().required()
 });
 
-server.get('/participants', (request, response) => {
-  console.log('pegou os nomes');
-  db.collection('participants')
-    .find()
-    .toArray()
-    .then(participantsList => response.send(participantsList));
+server.post('/participants', async (request, response) => {
+  const participant = request.body;
+  const validate = participantSchema.validate(participant, { abortEarly: false });
+  const { error } = validate;
+
+  if (error) {
+    const messages = error.details.map(error => error.message);
+    response.status(422).send(messages);
+    console.log('caiu aqui');
+    return;
+  }
+
+  try {
+    connectClient();
+
+    await db.collection('participants').insertOne(participant);
+    response.sendStatus(201);
+
+    closeClient();
+  } catch {
+    response.status(500).send(chalk.red('Não foi possível cadastrar seu usuário.'));
+    closeClient();
+  }
+});
+
+server.get('/participants', async (request, response) => {
+  try {
+    connectClient();
+
+    const participantsList = await db.collection('participants').find().toArray();
+    response.send(participantsList);
+    closeClient();
+  } catch {
+    response
+      .status(500)
+      .send(chalk.red('Não foi possível encontrar a lista de participantes.'));
+    closeClient();
+  }
 });
 
 server.listen(5000, () => {
-  console.log(chalk('Rodando na porta 5000'));
+  console.log(chalk.cyan('Rodando na porta 5000'));
 });
+
+async function connectClient() {
+  await client.connect();
+}
+
+function closeClient() {
+  client.close();
+}
